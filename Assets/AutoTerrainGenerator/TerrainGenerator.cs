e260b947f3166feaa0ece9d28d00e48f6df861ea
snsk <snskngo03.sch.2021@gmail.com>
2024-06-09 16:26:57 +0900

    make project

---
+++ Assets/AutoTerrainGenerator/TerrainGenerator.cs
@@ -0,0 +1,15 @@
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AutoTerrainGenerator
+{
+    public static class TerrainGenerator
+    {
+        public static void Generate()
+        {
+
+        }
+    }
+}

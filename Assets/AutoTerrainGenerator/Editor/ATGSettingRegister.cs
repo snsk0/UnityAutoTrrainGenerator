0d3954f187eb073798b77962f0291673786c59db
snsk <snskngo03.sch.2021@gmail.com>
2024-06-28 14:19:05 +0900

    ProjectSettingsにデータを追加

--- Assets/AutoTerrainGenerator/Editor/ATGSettingRegister.cs
+++ Assets/AutoTerrainGenerator/Editor/ATGSettingRegister.cs
@@ -1,28 +1,111 @@
 #if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-internal static class ATGSettingRegister
-{
-    private class ATGSettingsProvider : SettingsProvider
+namespace AutoTerrainGenerator.Editor {
+    internal static class ATGSettingRegister
     {
-        public ATGSettingsProvider(string path, SettingsScope scopes, IEnumerable<string> keywords) : base(path, scopes, keywords)
+        private class ATGSettingsProvider : SettingsProvider
         {
-            label = "AutoTerrainGenerator";
+            private ATGSettingData _settingData;
+            private SerializedObject _serializedObject;
+
+            public ATGSettingsProvider(string path, SettingsScope scopes, IEnumerable<string> keywords) : base(path, scopes, keywords)
+            {
+                label = "AutoTerrainGenerator";
+            }
+
+            public override void OnActivate(string searchContext, VisualElement rootElement)
+            {
+                _settingData = ATGSettingData.GetOrCreateData();
+                _serializedObject = new SerializedObject(_settingData);
+            }
+
+            public override void OnDeactivate()
+            {
+                if(_serializedObject != null)
+                {
+                    ATGSettingData.SetConfigData(_settingData);
+                }
+            }
+
+            public override void OnGUI(string searchContext)
+            {
+                _serializedObject.Update();
+
+                EditorGUILayout.PropertyField(_serializedObject.FindProperty(ATGSettingData.HeightMapGeneratorsName));
+
+                //classàÍóóÇéÊìæÇµÅAGeneratorà»äOnullÇë„ì¸Ç∑ÇÈ
+                bool isGenerator = false;
+                for(int i = 0; i < _settingData.heightMapGenerators.Count; i++)
+                {
+                    MonoScript script = _settingData.heightMapGenerators[i];
+                    if(script == null)
+                    {
+                        break;
+                    }
+
+                    Type scriptType = script.GetClass();
+                    foreach(Type iType in scriptType.GetInterfaces())
+                    {
+                        isGenerator = iType == typeof(IHeightMapGenerator);
+                        if (isGenerator)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (!isGenerator)
+                    {
+                        _settingData.heightMapGenerators[i] = null;
+                    }
+                }
+
+                _serializedObject.ApplyModifiedProperties();
+            }
+
+
         }
 
-        public override void OnGUI(string searchContext)
+        internal class ATGSettingData : ScriptableObject
         {
-            Debug.Log(searchContext);
+            public const string HeightMapGeneratorsName = "_heightMapGenerators";
+
+            [SerializeField]
+            private List<MonoScript> _heightMapGenerators;
+            public List<MonoScript> heightMapGenerators => _heightMapGenerators;
+
+            internal static ATGSettingData GetOrCreateData()
+            {
+                string dataJson = EditorUserSettings.GetConfigValue(nameof(ATGSettingData));
+                ATGSettingData settingData = CreateInstance<ATGSettingData>();
+
+                if (!string.IsNullOrEmpty(dataJson))
+                {
+                    JsonUtility.FromJsonOverwrite(dataJson, settingData);
+                }
+                else
+                {
+                    settingData._heightMapGenerators = new List<MonoScript>();
+                }
+                return settingData;
+            }
+
+            internal static void SetConfigData(ATGSettingData settingData)
+            {
+                string dataJson = JsonUtility.ToJson(settingData);
+                EditorUserSettings.SetConfigValue(nameof(ATGSettingData), dataJson);
+            }
         }
-    }
 
-
-    [SettingsProvider]
-    internal static SettingsProvider CreateSettingProvider()
-    {
-        return new ATGSettingsProvider("Project/", SettingsScope.Project, new[] { "AutoTerrainGenerator", "ATG" });
+        [SettingsProvider]
+        internal static SettingsProvider CreateSettingProvider()
+        {
+            return new ATGSettingsProvider("Project/", SettingsScope.Project, new[] { "AutoTerrainGenerator", "ATG" });
+        }
     }
 }
 #endif

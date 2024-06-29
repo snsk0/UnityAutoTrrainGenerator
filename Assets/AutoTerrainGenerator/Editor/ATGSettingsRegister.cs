b7cc9981ed1ff974a134a537d3e646b446343a6f
snsk <snskngo03.sch.2021@gmail.com>
2024-06-30 07:56:03 +0900

    リファクタリング

--- Assets/AutoTerrainGenerator/Editor/ATGSettingsRegister.cs
+++ Assets/AutoTerrainGenerator/Editor/ATGSettingsRegister.cs
@@ -6,7 +6,7 @@ using UnityEngine.UIElements;
 
 namespace AutoTerrainGenerator.Editors 
 {
-    internal static class ATGSettingRegister
+    internal static class ATGSettingsRegister
     {
         private class ATGSettingsProvider : SettingsProvider
         {
